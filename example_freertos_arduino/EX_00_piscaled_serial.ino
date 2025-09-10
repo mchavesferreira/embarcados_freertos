80592a85be0bf4a5b8666477c9360c9bbc1e082b
mchavesferreira <63993080+mchavesferreira@users.noreply.github.com>
2025-09-10 13:31:34 -0300

    Create EX_00_piscaled_serial.ino

---
+++ example_freertos_arduino/EX_00_piscaled_serial.ino
@@ -0,0 +1,41 @@
+/*inclusão das Biblioteca do FreeRTOS*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+/*Definição do Pino do LED*/
+#define LED 2
+
+/* Protótipo da Task*/
+void vTaskBlink(void *pvParameters ); 
+
+/*Função setup - é chamada apenas uma vez na inicialização */
+void setup() {
+
+  Serial.begin(9600);
+  
+  xTaskCreate(
+     vTaskBlink
+    ,  "TaskBlink"              /* Nome da Task */
+    ,  configMINIMAL_STACK_SIZE /* Stack Size, não se preocupe com esse valor agora. Vamos estudar mais pra frente*/
+    ,  NULL                     /* parametro passado para a task*/
+    ,  2                        /* Prioridade da task*/
+    ,  NULL);                   /* handle da task*/
+}
+
+/* Função loop */
+void loop() { 
+  vTaskDelay(pdMS_TO_TICKS(10000));    /* Delay de 10 segundos */
+  Serial.println("Oi");                 /* Print a mensagem "Oi" na serial*/
+}
+
+/* Task Blink LED */
+void vTaskBlink(void *pvParameters ){
+
+  (void) pvParameters;
+  pinMode(LED,OUTPUT);
+
+  while(1){
+    digitalWrite(LED,!digitalRead(LED));
+    vTaskDelay(pdMS_TO_TICKS(200));
+  }
+}
